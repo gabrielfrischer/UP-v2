d1130f1aba416376167f0441fb34fe67a082f85e
GabeF <gabrielfrischer@gmail.com>
2016-02-18 19:03:32 -0800

    2/18/16

--- js/init.js
+++ js/init.js
@@ -25,4 +25,40 @@ $(document).ready(function(){
     $('ul.tabs').tabs();
   });
 
+   // Start of popout function
+$(".popout1 .btn1").click(function() {
+  $(this).toggleClass("active");
+  $(this).closest(".popout1").find(".panel1").toggleClass("active");
+});
+$(document).click(function() {
+  $(".popout1 .panel1").removeClass("active");
+  $(".popout1 .btn1").removeClass("active");
+});
+$(".popout1 .panel1").click(function(event) {
+  event.stopPropagation();
+});
+$(".popout1 .btn1").click(function(event) {
+  event.stopPropagation();
+});
+
+// end of popout function
+
+   // Start of popout function
+$(".popout2 .btn2").click(function() {
+  $(this).toggleClass("active");
+  $(this).closest(".popout2").find(".panel2").toggleClass("active");
+});
+$(document).click(function() {
+  $(".popout2 .panel2").removeClass("active");
+  $(".popout2 .btn2").removeClass("active");
+});
+$(".popout2 .panel2").click(function(event) {
+  event.stopPropagation();
+});
+$(".popout2 .btn2").click(function(event) {
+  event.stopPropagation();
+});
+
+// end of popout function
+
 
